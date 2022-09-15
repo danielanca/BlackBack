8645a8d1366547461498f344af7b42435cb777a2
Daniel Anca <steptu94@gmail.com>
2022-09-16 01:45:06 +0300

    no message

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ var RoomChannels: newTableProps = {};
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://blackjackdanielback.herokuapp.com/',
     methods: ['GET', 'POST'],
   },
 });
